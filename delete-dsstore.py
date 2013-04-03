6ac9203560dadfe31918e94259bfa3683ff0c549
Angelito M. Goulart <angelito.goulart@biostore.com.br>
2013-04-03 13:04:00 -0300

    first version

---
+++ delete-dsstore.py
@@ -0,0 +1,63 @@
+#!/bin/python
+
+#
+# delete-dssstore.py
+#
+# A simple python script to delete .DS_Store files
+#
+# Angelito M. Goulart
+# www.angelitomg.com
+#
+# April/2013
+#
+
+import os
+import sys
+
+# Help function
+def showhelp():
+	print "Usage: delete-dsstore.py PATH"
+	print "Example: delete-dsstore.py /Users/angelito"
+
+
+if len(sys.argv) > 1:
+
+	# Check if parameter is a dir
+	if os.path.isdir(sys.argv[1]):
+
+		# Clear file counter
+		i = 0;
+
+		# Get path
+		path  = sys.argv[1];
+
+		# Runs through all files in the directory
+		for root, sub, files in os.walk(path):
+			for file in files:
+
+				# Checks if exists .DS_Store file
+				if file == ".DS_Store":
+
+					# Get full path of current .DS_Store file
+					fullpath = os.path.abspath(os.path.join(root, file))
+					print "Deleting " + fullpath
+
+					# Remove file
+					os.remove(fullpath)
+					i += 1
+
+		print str(i) + " files deleted";
+
+	elif sys.argv[1] == '--help':
+		
+		# Show help message
+		showhelp()
+
+	else:
+
+		print "Argument must be a valid directory"
+
+else:
+
+	showhelp()
+
